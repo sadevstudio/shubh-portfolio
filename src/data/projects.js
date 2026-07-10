0e4bb3376a262b9187829cbc261e4f531ac83c5c
Shubh Asawa <shubhdesigns3d@gmail.com>
2026-07-11 02:22:57 +0530

    add S

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,7 +5,7 @@ import india from "../assets/projects/india/projectData";
 import Project_01 from "../assets/projects/Project_01/projectData";
 import Project_02 from "../assets/projects/Project_02/projectData";
 import Aure_crossiant from "../assets/projects/Aure_crossiant/projectData";
-import Scoovia from "../assets/projects/scoovia/projectData";
+import Scoovia from "../assets/projects/Scoovia/projectData";
 import Mayoor from "../assets/projects/Mayoor/projectData";
 
 const projects = [
